ef1d0fd4d33de7a31fcf261ceec516d080eb03c6
ryan-txn <tanryan2002@gmail.com>
2025-04-11 22:33:22 +0800

    Fix own merge conflicts

--- project_1.srcs/sources_1/new/Oled_Data_Mux.v
+++ project_1.srcs/sources_1/new/Oled_Data_Mux.v
@@ -1,14 +1,9 @@
 `timescale 1ns / 1ps
 
 module Oled_Data_Mux (
-<<<<<<< Updated upstream
-    input clk, input [1:0] selection, 
-    input [15:0] oled_data_game, input [15:0] oled_data_help, input [15:0] oled_data_menu,
-=======
     input clk, input [1:0] selection, input level_done1, input level_done2,
     input [15:0] oled_data_game1, input [15:0] oled_data_game2, input [15:0] oled_data_game3,
     input [15:0] oled_data_help, input [15:0] oled_data_menu,
->>>>>>> Stashed changes
     output reg enable, output reg [15:0] oled_data);
     
     localparam GAME_1 = 2'b00,
@@ -26,9 +21,6 @@ module Oled_Data_Mux (
     always @ (posedge clk) begin
         if (selection == 1) begin
             enable <= 1'b1;
-<<<<<<< Updated upstream
-            oled_data <= oled_data_game;
-=======
                 case (game_selector)
                     GAME_1: begin
                         if (level_done)
@@ -44,7 +36,6 @@ module Oled_Data_Mux (
                         oled_data = oled_data_game3;
                     //GAME_WIN: oled_data = oled_data_win;
                 endcase
->>>>>>> Stashed changes
         end else if (selection == 2) begin
             enable <= 1'b0;
             oled_data <= oled_data_help;
